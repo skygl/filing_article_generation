eae5ae6947f1fe446a9185fa061393cc2b2fd45e
skygl <jupiter9501@gmail.com>
2023-06-15 14:00:49 +0900

    fix error

--- SummarizationModule.py
+++ SummarizationModule.py
@@ -244,12 +244,15 @@ class LSTMEncoder(nn.Module):
         # hidden, cell: [bs, 2, hidden_dim]
         # hidden, cell: [bs, 2*hidden_dim]
         # hidden, cell: [bs, 1, 2*hidden_dim]
+        # hidden, cell: [1, bs, 2*hidden_dim]
         hidden = hidden.transpose(0, 1)
         hidden = hidden.contiguous().view(-1, 2*self.hidden_dim)
         hidden = hidden.unsqueeze(1)
+        hidden = hidden.transpose(0, 1)
         cell = cell.transpose(0, 1)
         cell = cell.contiguous().view(-1, 2*self.hidden_dim)
         cell = cell.unsqueeze(1)
+        cell = cell.transpose(0, 1)
 
         return output, (hidden, cell)
 
